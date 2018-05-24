83138025d3e6b787e1e83ad3eed322340bf1e7ce
Seth Childers <sethchilders92@gmail.com>
2018-05-24 10:10:32 -0600

    Added testing

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,15 +3,26 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
+    tap.test('textbook-access', (test) => {
 
-        test.pass('potato');
-        test.pass('tomato');
-        test.fail('avacado');
-
-        test.end();
+        /* Check if the module 'Textbook Access' exists */
+        canvas.get(`/api/v1/courses/${course.info.canvasOU}/modules?search_term=textbook%20access`, (err, modules) => {
+            if (err) {
+                course.error(err);
+                test.end();
+                callback(null);
+                return;
+            }
+            /* If it does exist, double check its name and check if its position is 2 */
+            if (modules[0] !== undefined) {
+                test.equal('Textbook Access', modules[0].name);
+                test.equal(2, modules[0].position);
+            } else {
+                test.fail(`The module 'Textbook Access' was not found`)
+            }
+            /* End the test */
+            test.end();
+            callback(null);
+        });
     });
-
-    // Always call the callback in your childTests with just null
-    callback(null);
 };
